090a7ef76715643f636cd865dfd9a77569d4580e
BlackDandel10n <blackdandel10n@proton.me>
2025-07-12 14:08:04 +0330

    Add interface

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ A huffman encoder/decoder written in python
 huffman encoding is a srategy to compress text files using prefixing
 
 you can read more about it at [https://en.wikipedia.org/wiki/Huffman_coding](https://en.wikipedia.org/wiki/Huffman_coding)
+
+## Install
+
+```bash
+poetry install
+```
+
+## Usage
+
+### Compress
+
+```bash
+poetry run src/huffer [FILE]
+```
--- src/huffer/__main__.py
+++ src/huffer/__main__.py
@@ -1,3 +1,7 @@
+from argparse import ArgumentParser
+from sys import stdin
+from os.path import exists, isfile
+
 def get_character_frequency(string:str):
     counter = {}
     for char in string:
@@ -12,3 +16,44 @@ def get_file_character_frequency(path:str):
             for k in line_counter.keys():
                 counter[k] = counter.get(k, 0) + line_counter[k]
     return counter
+
+def get_file_details(path:str):
+    res = {
+        "exists": exists(path),
+        "is_file": False
+    }
+
+    if res["exists"]:
+        res["is_file"] = isfile(path)
+    
+    return res
+
+def main():
+    parser = ArgumentParser("huffer", description="A huffman encoder/decoder written in python")
+    parser.add_argument("FILE", type=str, nargs="*", help="the FILE(s) to be processed, if FILE is not provided or is - STDIN is used")
+    
+    args = parser.parse_args()
+
+    if not args.FILE:
+        char_freq = get_character_frequency(stdin.read())
+        print(char_freq)
+    else:
+        char_freq = None
+        for f in args.FILE:
+            if f == "-":
+                char_freq = get_character_frequency(stdin.read())
+            else:
+                file_details = get_file_details(f)
+                if not file_details["exists"]:
+                    print(f"{parser.prog}: {f}: No such file or directory")
+                    continue
+                if not file_details["is_file"]:
+                    print(f"{parser.prog}: {f}: Is a directory")
+                    continue
+
+                char_freq = get_file_character_frequency(f)
+            print(char_freq)
+            
+
+if __name__ == "__main__":
+    main()

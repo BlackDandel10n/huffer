777908b6d1e68574bca0246cc42b0331ed8b526f
BlackDandel10n <blackdandel10n@proton.me>
2025-07-16 13:31:44 +0330

    Add huff tree

--- src/huffer/__main__.py
+++ src/huffer/__main__.py
@@ -2,17 +2,25 @@ from argparse import ArgumentParser
 from sys import stdin
 from os.path import exists, isfile
 
+class HuffNode:
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+   
+
 def get_character_frequency(string:str):
     counter = {}
     for char in string:
         counter[char] = counter.get(char, 0) + 1
     return counter
 
+
 def get_stdin_character_frequency():
     try:
         return sorted([(k, v) for k,v in get_character_frequency(stdin.read()).items()], key=lambda x: x[1], reverse=True)
     except Exception:
         return []
+
 
 def get_file_character_frequency(path:str):
     counter = {}
@@ -22,6 +30,7 @@ def get_file_character_frequency(path:str):
             for k in line_counter.keys():
                 counter[k] = counter.get(k, 0) + line_counter[k]
     return sorted([(k, v) for k,v in counter.items()], key=lambda x: x[1], reverse=True)
+
 
 def get_file_details(path:str):
     res = {
@@ -34,6 +43,32 @@ def get_file_details(path:str):
     
     return res
 
+
+def get_huff_freq_node(left=None, right=None):
+    left_freq = left[1] if left else 0
+    left = left[0] if left else left
+    right_freq = right[1] if right else 0
+    right = right[0] if right else right
+    return (HuffNode(left, right), right_freq + left_freq)
+
+
+def get_huff_tree(char_freq_list):
+    if len(char_freq_list) == 0:
+        return None
+    
+    char_freq_list = char_freq_list[:]
+    if len(char_freq_list) == 1:
+        return HuffNode(None, char_freq_list.pop()[0])
+    
+    while len(char_freq_list) > 1:
+        right = char_freq_list.pop()
+        left = char_freq_list.pop()
+        char_freq_list.append(get_huff_freq_node(left, right))
+        char_freq_list.sort(key=lambda x: x[1], reverse=True)
+
+    return char_freq_list.pop()[0]
+
+
 def main():
     parser = ArgumentParser("huffer", description="A huffman encoder/decoder written in python")
     parser.add_argument("FILE", type=str, nargs="*", help="the FILE(s) to be processed, if FILE is not provided or is - STDIN is used")
@@ -42,7 +77,8 @@ def main():
 
     if not args.FILE:
         char_freq = get_stdin_character_frequency()
-        print(char_freq)
+        huff_tree = get_huff_tree(char_freq)
+        print(huff_tree)
     else:
         char_freq = None
         for f in args.FILE:
@@ -58,7 +94,9 @@ def main():
                     continue
 
                 char_freq = get_file_character_frequency(f)
-            print(char_freq)
+            
+            huff_tree = get_huff_tree(char_freq)
+            print(huff_tree)
             
 
 if __name__ == "__main__":
